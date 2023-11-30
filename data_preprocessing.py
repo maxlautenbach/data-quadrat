8b35d37396b0abe1984d38d59b0d3353fd593e85
Niklas Weidenfeller <77716996+niklasweidenfeller@users.noreply.github.com>
2023-11-30 14:57:10 +0100

    Tree and forest (#1)
    * Experiments for DT and RF regressors
    
    * Plotting Decision Tree + first try with pipeline
    
    * Naming Fixes
    
    * Hyperparameter Tuning
    
    * RandomForestRegressor
    
    * DecisionTree Betty Style for all columns
    
    * Plotting
    
    * Baseline work parameters
    
    * Hyperparameter
    
    * RF & DT
    
    * More playing around
    
    * Plotted Decision Tree
    
    * hyperopt
    
    * WIP
    
    * Plots
    
    * Plots again
    
    * Update Plots
    
    * RF notebook
    
    * update rf notebook
    
    * Clean Up
    
    ---------
    
    Co-authored-by: Babett Müller <pepsix3@web.de>
    Co-authored-by: maxlautenbach <mx@unity-mail.de>

--- data_preprocessing.py
+++ data_preprocessing.py
@@ -6,14 +6,16 @@ from sklearn.preprocessing import *
 
 def preprocess_data(path):
     energy_consumption_data = pd.read_csv(path, index_col=0)
+
+    energy_consumption_data = energy_consumption_data.replace(-999, np.NaN).dropna()
+
     train, test = train_test_split(energy_consumption_data, shuffle=False, test_size=0.2)
     columns = list(energy_consumption_data.columns)
-    train = train.replace(-999, np.NaN)
-    train = train.dropna()
     scaler = MinMaxScaler()
 
     for column in columns[:-3]:
-        train[[column]] = scaler.fit_transform(train[[column]])
-        test[[column]] = scaler.fit_transform(test[[column]])
+        scaler.fit(train[[column]])
+        train[[column]] = scaler.transform(train[[column]])
+        test[[column]] = scaler.transform(test[[column]])
 
     return train, test
