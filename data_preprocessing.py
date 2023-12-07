30fa352e723b87a4d8fc38f047544ddd0fe8d9cc
maxlautenbach <mx@unity-mail.de>
2023-12-07 12:54:55 +0100

    Preprocessing Clean Up

--- data_preprocessing.py
+++ data_preprocessing.py
@@ -5,13 +5,19 @@ from sklearn.preprocessing import *
 
 
 def preprocess_data(path):
-    energy_consumption_data = pd.read_csv(path, index_col=0)
-    train, test = train_test_split(energy_consumption_data, shuffle=False, test_size=0.2)
-    columns = list(energy_consumption_data.columns)
+    # Load collected data
+    residual_load_data = pd.read_csv(path, index_col=0)
+
+    # Split into train and test split
+    train, test = train_test_split(residual_load_data, shuffle=False, test_size=0.2)
+
+    # Delete missing values
+    columns = list(residual_load_data.columns)
     train = train.replace(-999, np.NaN)
     train = train.dropna()
-    scaler = MinMaxScaler()
 
+    # Normalize data
+    scaler = MinMaxScaler()
     for column in columns[:-3]:
         train[[column]] = scaler.fit_transform(train[[column]])
         test[[column]] = scaler.fit_transform(test[[column]])
